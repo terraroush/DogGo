1b6f38f01739ba1580a3262002d41059b6a1fbf7
Terra Roush <terra.roush@gmail.com>
2020-12-11 10:00:39 -0600

    owner model done, owner repo done, iowner repo done, owners controller done, owner getall done

---
+++ DogGo/Controllers/OwnersController.cs
@@ -0,0 +1,96 @@
+﻿using DogGo.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Controllers
+{
+    public class OwnersController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepo;
+
+        // ASP.NET will give us an instance of our Walker Repository. This is called "Dependency Injection"
+        public OwnersController(IOwnerRepository ownerRepository)
+        {
+            _ownerRepo = ownerRepository;
+        }
+
+        // GET: OwnersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: OwnersController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: OwnersController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OwnersController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnersController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnersController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnersController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnersController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DogGo/Repositories/OwnerRepository.cs
@@ -0,0 +1,61 @@
+﻿using DogGo.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DogGo.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly IConfiguration _config;
+        public OwnerRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Owner> GetAllOwners()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    SELECT Id, Email, [Name], Address, Phone,
+                    NeighborhoodId
+                    FROM Owner
+                ";
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Owner> owners = new List<Owner>();
+                    while (reader.Read())
+                    {
+                        Owner owner = new Owner
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Email = reader.GetString(reader.GetOrdinal("Email")),
+                            Name = reader.GetString(reader.GetOrdinal("Name")),
+                            Address = reader.GetString(reader.GetOrdinal("Address")),
+                            Phone = reader.GetString(reader.GetOrdinal("Phone")),
+                            NeighborhoodId = reader.GetInt32(reader.GetOrdinal("NeighborhoodId"))
+                        };
+                        owners.Add(owner);
+                    }
+                    reader.Close();
+                    return owners;
+                }
+            }
+        }
+    }
+    
+}
