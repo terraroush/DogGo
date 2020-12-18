86f6146cde95b9314cb4ec78b040a6f1cd7a84cc
Terra Roush <terra.roush@gmail.com>
2020-12-18 14:45:24 -0600

    custom default photo achieved

--- DogGo/Models/ViewModels/WalkerProfileViewModel.cs
+++ DogGo/Models/ViewModels/WalkerProfileViewModel.cs
@@ -9,5 +9,16 @@ namespace DogGo.Models.ViewModels
     {
         public Walker Walker { get; set; }
         public List<Walk> Walks { get; set; }
+        public string TotalTimeWalkedDisplay { get; set; }
+        public string WalkTimeDisplay
+        {
+            get
+            {
+                int totalWalkSeconds = Walks.Sum(w => w.Duration);
+                TimeSpan walkTime = TimeSpan.FromSeconds(totalWalkSeconds);
+                string walkTimeDisplay = $"{walkTime.Hours}hr {walkTime.Minutes}min";
+                return walkTimeDisplay;
+            }
+        }
     }
 }
