5bbf38f378b4c28c34b39280e339c5653f7b6958
Terra Roush <terra.roush@gmail.com>
2020-12-22 12:36:48 -0600

    added required annotations for the create owner form

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,18 @@ namespace DogGo.Models
     public class Owner
     {
         public int Id { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
         public string Name { get; set; }
+        [Required]
+        [StringLength(80, MinimumLength = 10)]
         public string Address { get; set; }
+        [Required]
+        [Phone]
         public string Phone { get; set; }
+        [Required]
         public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
         //public List<Dog> { get; set; }
